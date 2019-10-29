28af2755fd70a53374dc26fefa5eaec254f647a1
IsaiasPacheco <37090442+IsaiasPacheco@users.noreply.github.com>
2019-10-28 19:54:58 -0600

    Lectura de archivos

--- Main.java
+++ Main.java
@@ -1,41 +1,19 @@
+import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main{
 
     public static void main(String[] args) {
-        
-        Lista listaA[] = { new Lista("Isaias"), 
-                           new Lista("Hugo"),
-                           new Lista("Luis"),
-                           new Lista("Oscar"),
-                           new Lista("Einar"),
-                           new Lista("Ricardo"),
-                           new Lista("Daniel"),
-                           new Lista("Ariel"),
-                           new Lista("Jesus"),
-                           new Lista("Omar"),
-                           new Lista("Alejandro"),
-                           new Lista("Ivan"),
-                           new Lista("Jack"),
-                           new Lista("John"),
-                           new Lista("Sparrow"),
-                           new Lista("Joan"),
-                           new Lista("Joaquin")
-                        };
 
-        ListaPares listaB [] = { new ListaPares("Isaias", "Hugo"), 
-                                 new ListaPares("Isaias", "Luis"), 
-                                 new ListaPares("Isaias", "Oscar"),
-                                 new ListaPares("Isaias", "Einar"),
-                                 new ListaPares("Isaias", "Daniel"),
-                                 new ListaPares("John", "Sparrow"),
-                                 new ListaPares("John", "Joaquin"),
-                                 new ListaPares("John", "Ivan"),
-                                 new ListaPares("John", "Jack"),
-                                 new ListaPares("John", "Alejandro"),
-                                };
+        
+        ArrayList<Lista> listaA = new ArrayList<>();
+        ArrayList<ListaPares> listaB = new ArrayList<>();
+
+        llenarLista( listaA , listaB , args );
+
 
         //remover el que conoce de la lista
 
@@ -43,22 +21,22 @@ public class Main{
         int cont = 0;
 
 
-            for( int i=0; i<listaA.length; i++ ){
+            for( int i=0; i<listaA.size(); i++ ){
                 
             
                 int contadorConocidos = 0 ;
                 ArrayList<String> aux = new ArrayList<>();
     
-                for( int j = 0; j<listaB.length; j++){
-                    if( listaB[j].estaEnLista(listaA[i].getNombre() )  && listaA[i].getConsiderar() ){
+                for( int j = 0; j<listaB.size(); j++){
+                    if( listaB.get(j).estaEnLista(listaA.get(i).getNombre() )  && listaA.get(i).getConsiderar() ){
                         contadorConocidos++;
-                        aux.add(listaB[j].retornarConocido());
+                        aux.add(listaB.get(j).retornarConocido());
                         
                     }
                 }
     
-                if( contadorConocidos >= 5 && ( -contadorConocidos + listaA.length ) >= 5 ){
-                    listaInvitados.add(listaA[i].getNombre());
+                if( contadorConocidos >= 5 && ( -contadorConocidos + listaA.size() ) >= 5 ){
+                    listaInvitados.add(listaA.get(i).getNombre());
                     for(int k=0; k<aux.size(); k++){
                         cambiarEstado(aux.get(k), listaA);
                     }
@@ -71,12 +49,48 @@ public class Main{
 
     }
 
-    public static void cambiarEstado( String nombre, Lista[] lista ) {
-        for( int i=0; i< lista.length; i++ ){
-            if( nombre.equals(lista[i].getNombre())){
-                lista[i].setConsiderar(false);
+    public static void cambiarEstado( String nombre, ArrayList<Lista> lista ) {
+        for( int i=0; i< lista.size(); i++ ){
+            if( nombre.equals(lista.get(i).getNombre())){
+                lista.get(i).setConsiderar(false);
             }
         }
+    }
+
+    public static void llenarLista( ArrayList<Lista> listaA , ArrayList<ListaPares> listaB, String args[] ){
+
+        try{
+
+            File archivo = new File("pruebaCod\\" + args[0]);
+            FileReader fr = new FileReader (archivo);
+            BufferedReader br = new BufferedReader(fr);
+    
+            
+            String listaA_cad = br.readLine();
+
+            StringTokenizer st = new StringTokenizer(listaA_cad, ",");
+            while( st.hasMoreTokens() ){
+                listaA.add(new Lista(st.nextToken()));
+            }
+
+            String listaB_cad = br.readLine();
+
+            StringTokenizer st_b = new StringTokenizer(listaB_cad, ";");
+            
+            while( st_b.hasMoreTokens()){
+                StringTokenizer pares = new StringTokenizer(st_b.nextToken(), "-");
+                listaB.add(new ListaPares( pares.nextToken(), pares.nextToken() ));
+            }
+            
+            
+
+
+            br.close();
+            
+            }catch( Exception e ){
+                System.out.println(e);
+            }
+
     }
   
 
